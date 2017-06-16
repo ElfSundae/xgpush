65f9d7ad628be80936826afda5c22fa87222d517
Elf Sundae <elf.sundae@gmail.com>
2017-06-16 14:14:40 +0800

    Do not output error message

--- src/XingeApp.php
+++ src/XingeApp.php
@@ -1669,7 +1669,7 @@ class RequestBase
                 throw new Exception('CURL ERROR: ' . curl_error($curl_handle));
             }
         } catch (Exception $e) {
-            echo $e->getMessage();
+            // echo $e->getMessage();
         }
         curl_close($curl_handle);
 
